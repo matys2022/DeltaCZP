f9fc1827489c4d7595a0e9dc014d0b89cb73ce18
matys2022 <104504451+matys2022@users.noreply.github.com>
2025-10-16 07:57:59 +0200

    Update of CardGeneration -> Updated the card number generation

--- DeltaBank/src/main/java/org/mathiasus/utilities/generation/CardGeneration.java
+++ DeltaBank/src/main/java/org/mathiasus/utilities/generation/CardGeneration.java
@@ -4,15 +4,18 @@ import java.util.Random;
 import java.util.UUID;
 
 public class CardGeneration {
-    public CardGeneration() {
+    public CardGeneration() {}
 
-    }
+
     public String generateAccountNumber(){
-        return UUID.randomUUID().toString().replace("-", "");
+        String formatted = UUID.randomUUID().toString().replace("-", "").toUpperCase();
+        long parsed1 = Long.parseUnsignedLong(formatted.substring(0, 8), 16);
+        long parsed2 = Long.parseUnsignedLong(formatted.substring(9, 16), 16);
+        return  (parsed1 + "" + parsed2);
     }
-    public String generateUUID(){
-        return UUID.randomUUID().toString();
-    }
+
+    public String generateUUID(){return UUID.randomUUID().toString();}
+
     public String generateCardCVV(){
         return new Random().nextInt(999)+"";
     }
